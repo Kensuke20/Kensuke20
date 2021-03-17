664480d9fbf43c0c5607d1e04709aee54536c29c
ohno <p4ma8f15@s.okayama-u.ac.jp>
2021-03-17 17:10:34 +0900

    modified: TexTree.java
    表を出力する処理を追加した．

--- TexTree.java
+++ TexTree.java
@@ -1,12 +1,21 @@
 import java.io.*;
 class TexTree{
+	public static final int MAX_STORE = 20;
+	public static NameAndVal[] nameTable = new NameAndVal[MAX_STORE];
+
 	public static void main(String[] args){
-		String s;
-		int tmp, count = 0;
+		
+		String s, new_s;
+		int tmp, count = 0, tableNum;
 		Stack stack = new Stack();
 		SNode top = new SNode();
 		
 		top = stack.StackCreate();
+		
+		for(int i = 0; i < nameTable.length; i++){
+			nameTable[i] = new NameAndVal();
+		}
+		
 		
 		try{
 			BufferedReader fin = new BufferedReader(new FileReader(args[0]));
@@ -14,27 +23,98 @@ class TexTree{
 			while((s = fin.readLine()) != null){
 				tmp = s.indexOf("\\begin");
 				if(tmp != -1){
+					/////////////////////////////////////////////////
+					
 					count++;
 					stack.push(top, count);
 					insertTab(count, 0);
 					System.out.println(count + ": " + s.substring(tmp));
 					insertTab(count + 1, 1);
+					
+					/////////////////////////////////////////////////
+					
+					new_s = cutOutChar(s);
+					tableNum = checkTable(new_s);
+					if(tableNum < 0){
+						tableNum = registTable(new_s);
+						nameTable[tableNum].val_begin++;
+					}
+					else{
+						nameTable[tableNum].val_begin++;
+					}
+					///////////////////////////////////////////////////
 				}
 				
 				tmp = s.indexOf("\\end");
 				if(tmp != -1){
+					/////////////////////////////////////////////////
 					insertTab(count, 0);
 					System.out.println(stack.pop(top) + ": " + s.substring(tmp));
 					insertTab(count, 1);
 					count--;
+					
+					/////////////////////////////////////////////////
+					new_s = cutOutChar(s);
+					tableNum = checkTable(new_s);
+					if(tableNum < 0){
+						tableNum = registTable(new_s);
+						nameTable[tableNum].val_end++;
+
+					}
+					else{
+						nameTable[tableNum].val_end++;
+					}
+					///////////////////////////////////////////////////
 				}
 			}
 			fin.close();
+			
+			System.out.println("---------------------------------------------------------");
+			System.out.printf("             \\begin  \\end\n");
+			System.out.println("---------------------------------------------------------");
+			for(int n = 0; n < nameTable.length; n++){
+				if(!nameTable[n].name.equals("noname")){
+					System.out.printf("%-12s:  %3d    %3d\n", nameTable[n].name, nameTable[n].val_begin, nameTable[n].val_end);
+				}
+			}
+			System.out.println("---------------------------------------------------------");
 		}
 		catch(Exception e){
 			System.out.print(e);
 		}
 	}
+	
+	static String cutOutChar(String input_s){
+		int a, b;
+		String output_s;
+		
+		a = input_s.indexOf('{');
+		b = input_s.indexOf('}');
+		output_s = input_s.substring(a + 1, b);
+		
+//		System.out.println(output_s + "  This is name.");
+		return output_s;
+	}
+	
+	static int checkTable(String name){
+		for(int n = 0; n < nameTable.length; n++){
+			if(name.equals(nameTable[n].name)){
+				return n;
+			}
+		}
+		return -1;
+	}
+	
+	static int registTable(String newName){
+		for(int i = 0; i < nameTable.length; i++){
+			if(nameTable[i].name.equals("noname")){
+				nameTable[i].name = newName;
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	
 	static void insertTab(int count_tab, int count_newline){
 		int n, m;
@@ -45,6 +125,13 @@ class TexTree{
 			System.out.println();
 		}
 	}
+}
+
+
+class NameAndVal{
+	String name = "noname";
+	int val_begin;
+	int val_end;
 }
 
 class SNode{
